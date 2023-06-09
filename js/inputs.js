849fbd95f7c36ac5e5661874bc18cb55ca02f9f7
Nicolas Ollier <nicolas@lepetitmartin.com>
2023-06-09 15:03:10 +0200

    creates factory for filters inputs

--- js/inputs.js
+++ js/inputs.js
@@ -1,3 +1,5 @@
+import { SortInputFactory } from "./factories/sortInputFactory.js";
+
 // Watch user input and emit to index.js to update recipes to display
 export const searchInputHandler = document.querySelector("#search-input").addEventListener("input", (e) => {
   const updateWordListEvent = new CustomEvent("updateWordList", {
@@ -7,3 +9,13 @@ export const searchInputHandler = document.querySelector("#search-input").addEve
   });
   window.dispatchEvent(updateWordListEvent); // Dispatching the created event
 });
+
+// Render the dropdown lists
+const ingredientsInputFactory = new SortInputFactory("ingredients");
+ingredientsInputFactory.renderListData(null);
+
+const appliancesInputFactory = new SortInputFactory("appliances");
+appliancesInputFactory.renderListData(null);
+
+const ustensilsInputFactory = new SortInputFactory("ustensils");
+ustensilsInputFactory.renderListData(null);
--- js/sorts/sortByIngredients.js
+++ js/sorts/sortByIngredients.js
@@ -1,6 +1,10 @@
 // Import recipesByIngredients from initializeRecipeMaps.js
 import { recipesByIngredients } from '../initializeRecipeMaps.js';
+import { recipes } from '../recipes.js';
 
 export const sortByIngredients = (userInput) => {
-    return recipesByIngredients[userInput];
+  if (userInput.length < 3 || !recipesByIngredients[userInput]) {
+    return recipes;
+  }
+  return recipesByIngredients[userInput];
 }
