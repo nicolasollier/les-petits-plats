0d19f8d73d2a6d2ec21e4b219e6bfa1632ea034a
Nicolas Ollier <nicolas@lepetitmartin.com>
2023-07-19 18:41:04 +0200

    translates generateHashmap() into imperative

--- js/initializeRecipeMaps.js
+++ js/initializeRecipeMaps.js
@@ -10,31 +10,43 @@ function generateHashmap(recipes) {
   recipesByAppliance = {};
 
   // Creates the hasmaps by iterating over recipes.js
-  recipes.forEach((recipe) => {
+  for(let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+
     // Creates a hashmap of recipes by appliance
     const normalizedAppliance = recipe.appliance.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
 
-    !recipesByAppliance[normalizedAppliance] && (recipesByAppliance[normalizedAppliance] = []);
+    if(!recipesByAppliance[normalizedAppliance]) {
+      recipesByAppliance[normalizedAppliance] = [];
+    }
     recipesByAppliance[normalizedAppliance].push(recipe);
     
     // Creates a hashmap of recipes by ingredient
-    recipe.ingredients.forEach((ingredient) => {
+    for(let j = 0; j < recipe.ingredients.length; j++) {
+      const ingredient = recipe.ingredients[j];
+
       // Remove accents and convert to lowercase
       const normalizedIngredient = ingredient.ingredient.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
     
-      !recipesByIngredient[normalizedIngredient] && (recipesByIngredient[normalizedIngredient] = []);
+      if(!recipesByIngredient[normalizedIngredient]) {
+        recipesByIngredient[normalizedIngredient] = [];
+      }
       recipesByIngredient[normalizedIngredient].push(recipe);
-    });
+    }
     
     // Creates a hashmap of recipes by utensil
-    recipe.ustensils.forEach((utensil) => {
+    for(let k = 0; k < recipe.ustensils.length; k++) {
+      const utensil = recipe.ustensils[k];
+
       // Remove accents and convert to lowercase
       const normalizedUtensil = utensil.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
 
-      !recipesByUstensil[normalizedUtensil] && (recipesByUstensil[normalizedUtensil] = []);
+      if(!recipesByUstensil[normalizedUtensil]) {
+        recipesByUstensil[normalizedUtensil] = [];
+      }
       recipesByUstensil[normalizedUtensil].push(recipe);
-    });
-  });
+    }
+  }
 
   //Creates custom event to update recipes could thrown an error or take loading params
   const event = new CustomEvent("updateHashmap");
